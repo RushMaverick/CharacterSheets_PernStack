1d8ba31ee4aecb8d694da6572a81cb2c85db0cf4
Rasmus Rask <rasse.rask@gmail.com>
2025-02-17 11:05:19 +0200

    feat(App.tsx): Contain components as well as handle sheet modal opening and closing.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 
 //Components
-import AddModal from "./components/AddModal";
+import AddSheetModal from "./components/AddSheetModal";
+import SheetModal from "./components/SheetModal";
 
 const App: React.FC = () => {
+	const [isModalOpen, setIsModalOpen] = useState<boolean>(false); //change to Zustand
+
+	const openModal = () => {
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
+
 	return (
 		<div>
-			<AddModal />
+			<AddSheetModal onClick={() => openModal()} />
+			<SheetModal isOpen={isModalOpen} onClose={closeModal} />
 		</div>
 	);
 };
